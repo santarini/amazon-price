863f66e0161117b9b780e7b7473425e5a5d4a77b
Makoa <32736507+santarini@users.noreply.github.com>
2018-05-07 17:23:48 -1000

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -1,6 +1,7 @@
-#!Python 3
+#1Python 3
 
 import time
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -12,11 +13,14 @@ driver = webdriver.Chrome(r"C:\Program Files\Python\Python36\chromedriver.exe")
 
 #navigate to careers webpage
 driver.get('https://www.amazon.com/s/page=1&keywords=cashews')
-time.sleep(1)
 
 pageHTML = driver.find_element_by_tag_name('html')
 pageHTML = pageHTML.get_attribute('innerHTML')
 soup = bs.BeautifulSoup(pageHTML, 'lxml')
 container = soup.find("ul", {"id": "s-results-list-atf"})
-
-print(container)
+i = 1
+for row in container.findAll("div", {"class": "a-fixed-left-grid-inner"}):
+    print("************"+ str(i)+ "********")
+    print(row.text)
+    i +=1
+webdriver.Close()
